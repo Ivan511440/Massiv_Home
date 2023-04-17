4ed25178c7ba357da3ca3f2c572e622fa6aa2373
Ivan511440 <128604283+Ivan511440@users.noreply.github.com>
2023-04-17 18:30:11 +0600

    Zadacha3

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте двумерный массив из целых чисел.
+// Найдите средне арифметическое элементов в каждом столбце.
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+using System;
+using static System.Console;
+
+Random random = new Random();
+int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+   for (int j = 0; j < arr.GetLength(1); j++)
+   {
+    arr[i, j] = random.Next(1, 10);
+    Write(arr[i, j] + " ");
+   }
+   WriteLine();
+}
+WriteLine("------------------");
+WriteLine(arr.GetLength(0));
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Write($"{sum / arr.GetLength(0)}");
+}
+ReadLine();
+
+
+
+
+
+
