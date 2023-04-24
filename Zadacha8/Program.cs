199e60fc615d73aa796fd609d30a675d65a6f535
Ivan511440 <128604283+Ivan511440@users.noreply.github.com>
2023-04-24 11:00:18 +0600

    Zadacha8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,48 @@
+﻿// Напишите программу, которая заполнит спирально массив размером 4 на 4.
+// Например, на выходе получается вот такой массив:
+
+// 01 02 03 04
+// 12 13 14 05
+// 11 15 16 06
+// 10 09 08 07
+
+using System;
+using static System.Console;
+
+WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(ReadLine());
+
+int[,] nums = new int[size, size];
+
+int num = 1;
+int i = 0;
+int j = 0;
+
+while (num <= size * size)
+{
+    nums[i, j] = num;
+    if (i <= j + 1 && i + j < size - 1) ++j;
+    else if (i < j && i + j >= size - 1) ++i;
+        else if (i >= j && i + j > size - 1) --j;
+        else
+            --i;
+        ++num;
+}
+
+PrintArray(nums);
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Write("[");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write(array[i, j] + " ");
+        }
+        Write("]");
+        WriteLine("");
+    }
+}
+
+
